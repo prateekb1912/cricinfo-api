8926a87e8a23d54550ef77c03232d847a686baef
prateek-ml <prateekb1912@gmail.com>
2021-07-20 10:28:14 +0530

    Check available players from grid div

--- spiders/active_players.py
+++ spiders/active_players.py
@@ -8,10 +8,16 @@ class PlayersSpider(scrapy.Spider):
     name = 'players_spider'
 
     def start_requests(self):
-        urls = []
-
-        for i in range(100):
-            urls.append(f"https://www.espncricinfo.com/ci/content/player/country.html?country={i}")
+        url = "https://www.espncricinfo.com/ci/content/player/country.html?country=160"
         
-        for url in urls:
-            yield scrapy.Request(url, callback=self.parse)
+        yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        if response.status == 200:
+            container = response.css("div.player-index-container")
+
+            # Checking if players info are available for the team
+            players_grid = container.css("div.player-index-grid")
+
+            if len(players_grid > 0):
+               team = container.css("h2::text").get()
