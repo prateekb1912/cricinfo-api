07d83a7df78a7d5e27463ec1cfebda95b024e20e
prateek-ml <prateekb1912@gmail.com>
2021-10-13 15:57:32 +0530

    Add all the match details into the table

--- scorecard.py
+++ scorecard.py
@@ -4,6 +4,7 @@
 
 import requests
 from utils import *
+from datetime import datetime
 
 
 # We will use the 52nd match of the IPL 2021 (latest as of now) for sampling purposes
@@ -28,20 +29,51 @@ c.execute("""
         match_id INT PRIMARY KEY,
         date TEXT,
         time TEXT,
-        team1_id INT,
-        team2_id INT,
+        ground INT,
+        team1 INT,
+        team2 INT,
         toss_winner INT,        
         toss_decision TEXT,
         winner INT,
-        result TEXT,
-        player_of_match TEXT,
-    );
+        result TEXT,    
+        UNIQUE(match_id) ON CONFLICT REPLACE
+        );
 """)
 
 
 response = requests.get(url, headers=headers)
 
 data = response.json()
+
+
+## Getting all the match details before diving into the batting and bowling scorecards
+match_obj = data['match']
+
+match_details = dict()
+
+match_details['series_id'] = match_obj['series']['objectId']
+match_details['season'] = match_obj['season']
+match_details['match_id'] = match_obj['objectId']
+
+date_time = match_obj['startTime']
+d = datetime.fromisoformat(date_time[:-1])
+
+match_details['date'] = d.strftime("%Y-%m-%d")
+match_details['time'] = d.strftime("%H-%M-%S")
+
+match_details['ground'] = match_obj['ground']['objectId']
+match_details['team1'] = match_obj['teams'][0]['team']['objectId']
+match_details['team2'] = match_obj['teams'][1]['team']['objectId']
+match_details['toss_winner'] = match_obj['tossWinnerTeamId']
+match_details['toss_decision'] = 'bat' if match_obj['tossWinnerChoice'] == 1 else 'bowl'
+match_details['winner'] = match_obj['winnerTeamId']
+match_details['result'] = match_obj['statusText']
+
+
+c.execute('''
+    INSERT INTO match_details 
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?)''', tuple(match_details.values()))
+
 
 scorecard = data['content']['scorecard']
 
