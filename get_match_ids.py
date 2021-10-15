e33d33f12f2ecd3efa96297cf03b02f75a23c242
prateek-ml <prateekb1912@gmail.com>
2021-10-15 14:59:38 +0530

    Remove database and change the table columns

--- get_match_ids.py
+++ get_match_ids.py
@@ -22,9 +22,9 @@ c = conn.cursor()
 # Creating a table with league detaisl like title, ID, number of seasons
 c.execute("""
     CREATE TABLE IF NOT EXISTS leagues(
-        id INT PRIMARY KEY,
-        title TEXT,
-        seasons INT
+        season_id INT PRIMARY KEY,
+        league_title TEXT,
+        season INT
     );
 """)
 
@@ -34,46 +34,7 @@ c.execute("""
         league_id INT,
         match_id INT PRIMARY KEY,
         FOREIGN KEY (league_id)
-            REFERENCES leagues(id)
+            REFERENCES leagues(season_id)
     );
 """)
 
-# Finding the specific list with all T20 league titles and IDs 
-leagues_list = soup.find_all('ul', attrs={'class': 'Record'})[11]
-leagues_links = [{link.text:int(re.findall("[0-9]+",link['href'])[0])} for link in leagues_list.find_all('a')]
-
-for ll in leagues_links:
-    league_name = list(ll.keys())[0]
-    if league_name == 'ICC Intercontinental Cup':
-        continue
-    league_id = list(ll.values())[0]
-    
-    results_link = header+f"ci/engine/records/team/match_results_season.html?id={league_id};type=trophy"
-    
-    res_page = requests.get(results_link)
-    soup = BeautifulSoup(res_page.content, 'html.parser')
-    
-    recTable = soup.find('table', attrs={'class':'recordsTable'})
-    
-    season_links = [link['href'] for link in recTable.find_all('a')]
-
-    num_seasons = len(season_links)
-
-    for s in season_links:
-        season_page = requests.get(header+s)
-        soup = BeautifulSoup(season_page.content, 'html.parser')
-
-        matchTable = soup.find('table', attrs = {'class': 'engineTable'})
-        matchLinks = matchTable.find_all('a', string = 'T20')
-
-        matchIDs = [int(re.findall("[0-9]+", link['href'])[0]) for link in matchLinks]
-
-        for id in matchIDs:
-            c.execute("""
-                INSERT INTO matches VALUES (?,?)""", (league_id, id))
-
-    
-    c.execute("""
-        INSERT INTO leagues VALUES (?,?,?)""", (league_id, league_name, num_seasons))
-    
-conn.commit()
